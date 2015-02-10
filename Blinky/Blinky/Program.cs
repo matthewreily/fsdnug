c6f976f014bada9ba9db6eb8a69571e8c4fcd064
matthewreily <me@mattreily.com>
2015-02-09 19:15:20 -0600

    initial commit

---
+++ Blinky/Blinky/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Blinky
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            var onboardpin = new OutputPort(Pins.GPIO_PIN_D5, true);
+            while (true)
+            {
+                onboardpin.Write(false);
+                Thread.Sleep(500);
+                onboardpin.Write(true);
+                Thread.Sleep(500);
+            }
+            // write your code here
+
+
+        }
+
+    }
+}
